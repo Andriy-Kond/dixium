cafd58bc4681105599b7c0df9d56f6432efdf487
Andrii <andrii.kondratovich@gmail.com>
2025-03-26 12:28:50 +0200

    Fix Vercel deployment

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import "styles/main.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/dixium">
+    <BrowserRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
